24f513a68452b368b985e12a0b6a210ce39939be
cleysonmagnos <cleyson_magnos1@hotmail.com>
2020-02-06 22:22:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
-<html lang="pt-br">
-  <head>
-    <title>Título da página</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <?php
-    $teste = true;
-    $array = array('alfredjudokus100289', 'aten', 'winsock', 'commanderroot');
-    $channel = $_GET['channel'];
-    while($teste){
-		$retorno = trim(file_get_contents("https://2g.be/twitch/randomviewer.php?channel=$channel"));
 
-		if(!in_array($retorno, $array)){
-			echo $retorno;
-			$teste = false;
-		}
-    }
-	//var_dump($teste); die;
-	?>
-  </body>
-</html>
+<?php
+$teste = true;
+$array = array('alfredjudokus100289', 'aten', 'winsock', 'commanderroot');
+$channel = $_GET['channel'];
+while($teste){
+	$retorno = trim(file_get_contents("https://2g.be/twitch/randomviewer.php?channel=$channel"));
 
- 
+	if(!in_array($retorno, $array)){
+		echo $retorno;
+		$teste = false;
+	}
+}
+//var_dump($teste); die;
+?>
+
